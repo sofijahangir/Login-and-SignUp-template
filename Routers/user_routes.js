80d02273eaf11a3772777bde792e94ba9fc899f9
jay459 <anigounigoud@gmail.com>
2020-12-30 13:15:41 +0530

    Auth Route

--- Routers/user_routes.js
+++ Routers/user_routes.js
@@ -1,9 +1,17 @@
 const user_controllers = require("./../Controllers/user_controllers");
+const auth = require("./../Middleware/auth");
 const  express = require('express');
 const router = express.Router();
 
 router.post('/signup' , user_controllers.signup);
 
 router.post('/login' , user_controllers.login);
+
+router.get('/auth', auth ,(req, res) => {
+    res.json({
+        message: 'Auth success'
+    });
+});
+
 
 module.exports = router;
